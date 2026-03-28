3f851a45d3b45d00bd09eb0148a6a6f7e91e5b47
flyzzz <hejunfei2005@163.com>
2026-03-28 12:33:47 +0800

    new .h file

---
+++ ds18b20.h
@@ -0,0 +1,31 @@
+/********************************************************************************
+ *      Copyright:  (C) 2026 qyc
+ *                  All rights reserved.
+ *
+ *       Filename:  ds18b20.h
+ *    Description:  This file 
+ *
+ *        Version:  1.0.0(27/03/26)
+ *         Author:  qingyuechuan <360744619@qq.com>
+ *      ChangeLog:  1, Release initial version on "27/03/26 12:59:00"
+ *                 
+ ********************************************************************************/
+
+
+#ifndef DS18B20_H
+#define DS18B20_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+
+int read_temperature(float *temperature);
+
+#ifdef __cplusplus
+
+}
+#endif
+
+#endif
