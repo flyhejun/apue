bb9e90d6d01d7a7f880a3f1af64b63cfe5178680
flyzzz <hejunfei2005@163.com>
2026-03-28 12:15:58 +0800

    new .c file

--- ds18b20.c
+++ ds18b20.c
@@ -1,5 +1,3 @@
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,82 +6,118 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <dirent.h>
-#include <dirent.h>
+#include "ds18b20.h"
 
+#define W1_BASE_PATH 	"/sys/bus/w1/devices/"
 
-int main (int argc ,char *argv[])
+static int find_sn(char *sn, size_t sn_len)
 {
-         int            fd = -1;
-	 char           buf[128];
-	 char          *ptr = NULL;
-	 float          temp;
-	 DIR           *dirp = NULL;
-	 struct dirent *direntp = NULL;
-	 char           w1_path[64] = "/sys/bus/w1/devices/";
- 	 char           chip_sn[64];
-	 int            found = 0;
+	DIR				*dirp = NULL;
+	struct dirent 	*direntp = NULL;
+	int 			found = 0;
+	if(sn == NULL || sn_len == 0)
+	{
+		printf("sn/sn_len can't be NULL\/0\n");
+		return -6;
+	}
 
-	 dirp  = opendir(w1_path);
-	 if( !dirp )
-	 {
-	     printf("open floder %s failure : %s\n", w1_path, strerror(errno));
-	     return -1;
-	 }
-	 
+	dirp = opendir(W1_BASE_PATH);
+	if(!dirp)
+	{
+		printf("open file % s failure: %s\n", W1_BASE_PATH, strerror(errno));
+		return -1;
+	}
 
-	 while( NULL != ( direntp = readdir(dirp)) )
-	 {   
-             if( strstr( direntp->d_name, "28-"))
-	     {
-		     strncpy(chip_sn, direntp->d_name, sizeof(chip_sn));
-	             found = 1;
-	     }
-	 }
+	while((direntp = readdir(dirp)) != NULL)
+	{
+		if(strstr(direntp->d_name, "28-"))
+		{
+			strncpy(sn, direntp->d_name, sn_len);
+			sn[sn_len - 1] = '\0';
+			found = 1;
+			break;
+		}
+	}
 
-	 closedir(dirp);
+	closedir(dirp);
 
-         if( !found )
-	 {
-	     printf("cant find ds18b20 chipset\n");
-	     return -2;
-	 }
+	if(!found)
+	{
+		printf("find "28-" file faliure: %s\n", strerror(errno));
+		return -2;
+	}
 
-	 strncat(w1_path, chip_sn, sizeof(w1_path)-strlen(w1_path));
-	 strncat(w1_path, "/w1_slave", sizeof(w1_path)-strlen(w1_path));
-
-	 fd = open(w1_path, O_RDONLY);
-	 if( fd < 0)
-	 {
-	     printf("open file failure: %s\n", strerror(errno));
-	     perror("open file failure.");
-	     return -3;
-	 }
-
-	 memset(buf, 0, sizeof(buf));
-  	 read (fd, buf, sizeof(buf));
-
-	 if( read(fd, buf, sizeof(buf)) < 0)
-	 {
-	     printf("read data from fd=%d failure %s\n", fd, strerror(errno));
-	     return -4;
-	 }
-
-	 ptr = strstr(buf, "t=");
-
-	 if ( !ptr )
-	 {
-	    printf("can't find\n");
-	    return -5;
-	 
-	 }
-
-	 ptr += 2;
-	 
-         temp = atof(ptr)/1000;
-         printf("tempreture: %f\n", temp);
-	 close (fd);
-	
-
-
+	return 0;
 
 }
+
+
+static int read_data(const char *sn, char *buf, size_t buf_len)
+{
+	char 		w1_path[128];
+	int			file_fd = -1;
+	ssize_t 	rt;
+
+	memset(w1_path, 0, sizeof(w1_path));
+	strncpy(w1_path, W1_BASE_PATH， sizeof(w1_path));
+	strncat(w1_path, sn, sizeof(w1_path)-strlen(w1_path));
+	strncat(w1_path, "/w1_slave", sizeof(w1_path)-strlen(w1_path));
+
+	fd = open(w1_path, O_RDONLY);
+	if(fd < 0)
+	{
+		printf("open file %s failure: %s\n", w1_path, strerror(errno));
+		return -1;
+	}
+
+	memset(buf, 0, buf_len);
+	rt = read(file_fd, buf, buf_len);
+	if(rt <= 0)
+	{
+		printf("read data from fd[%d] failure: %s\n", file_fd, strerror(errno));
+		close(file_fd);
+		return -2;
+	}
+
+	close(file_fd);
+	return 0;
+}
+
+int 	read_temperature(float *temperature)
+{
+	char		sn[64];
+	char		buf[128];
+	int			ret;
+	char		*ptr = NULL;
+
+	if(temperature == NULL)
+	{
+		printf("temperature pointer is NULL\n");
+		return -6;
+	}
+
+	ret = find_sn(sn, sizeof(sn));
+	if(ret < 0)
+	{	
+		return ret;
+	}
+
+	ret = read_data(sn, buf, sizeof(buf));
+	if(ret < 0)
+	{
+		return ret-10;
+	}
+
+	ptr = strstr(buf, "t=");
+	if(!ptr)
+	{
+		printf("find temperature failure: %s\n", strerror(errno));
+		return -5;
+	}
+
+	ptr += 2;
+	*temperature = atof(ptr)/1000.0;
+	
+	return 0;
+}
+
